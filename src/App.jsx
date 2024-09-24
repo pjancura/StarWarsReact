040c3cf4dd087910665aa4af9225fcd37a3584da
ccolkereybel <163072600+ccolkereybel@users.noreply.github.com>
2024-09-24 18:29:24 -0400

    update Header file path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./Header.jsx";
+import Header from "./Header/Header.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
