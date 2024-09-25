acc9e8db8afe52d82cb411fc226788f4f7144567
pjancura <jancura.paul@gmail.com>
2024-09-25 09:21:17 -0400

    changes file structure for the formFilter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./Header/Header.jsx";
-import FormFilter from "./FormFilter/FormFilter.jsx";
+import FormFilter from "./Main/FormFilter.jsx";
 
 function App() {
   return (
