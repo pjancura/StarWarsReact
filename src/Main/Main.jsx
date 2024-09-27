9fe889298469c0586e2c0d30b76c603015d6e4bb
pjancura <jancura.paul@gmail.com>
2024-09-27 09:10:06 -0400

    changed alert() in Main.jsx

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -66,7 +66,7 @@ export default function Main() {
               return;
             }
             setError(e);
-            alert(e);
+            // alert(e);
           } finally {
             setIsLoading(false);
           }
